9c91338df0bb52da1d7ea91d03e810acaad4d1fc
keval <keval.waghate@gmail.com>
2023-11-10 22:51:16 +0530

    tax calculation added

---
+++ Taxcalculation.js
@@ -0,0 +1,23 @@
+const NumberOfConnections = document.getElementById("numbers_of_connections");
+const WaterVolume = document.getElementById("volume_of_water_usage");
+const WaterRate = document.getElementById("rate_of_water");
+const resultElement = document.getElementById("result");
+const btnCalculateWaterTax = document.getElementById("btnCalculateWaterTax");
+
+btnCalculateWaterTax.addEventListener('click', function (e) {
+    const connections = parseFloat(NumberOfConnections.value);
+    const volume = parseFloat(WaterVolume.value);
+    const rate = parseFloat(WaterRate.value);
+
+    // Check if the input values are valid numbers
+    if (isNaN(connections) || isNaN(volume) || isNaN(rate)) {
+        alert("Please enter valid numbers for all fields.");
+        return;
+    }
+
+    // Calculate the water tax
+    const waterTax = connections * volume * rate;
+
+    // Display the result on the HTML page
+    resultElement.textContent = "Amount of Tax : " + waterTax.toFixed(2);
+});
