88d15d266107137dc27aa76508ba144c69a777b6
keval <keval.waghate@gmail.com>
2023-11-22 08:13:59 +0530

    work done for payment

--- Taxcalculation.js
+++ Taxcalculation.js
@@ -49,13 +49,84 @@ btnCalculateWaterTax.addEventListener('click', function (e) {
 });
 
 // Function to save tax information to Firebase
-function saveTaxToFirebase(userName, taxAmount) {
+// function saveTaxToFirebase(userName, taxAmount) {
+//     const d = new Date();
+//     let currentMonthIndex = d.getMonth() + 1;
+//     let currentYear = d.getFullYear();
+
+//     // Reference to the user's tax data
+//     var userTaxRef = ref(database, `users/${userName}/waterTax/${currentMonthIndex}-${currentYear}`);
+
+//     // Push the new tax information to the user's tax data
+//     set(userTaxRef, {
+//         currentMonthIndex: `${currentMonthIndex}.${currentYear}`,
+//         taxAmount: taxAmount
+//     });
+
+//     // Display success message or perform any other actions
+//     console.log("Tax information saved to Firebase for user with userName: " + userName);
+// }
+
+// Function to retrieve a user based on userName
+function getUserByUserName(userName, callback) {
+    var usersRef = ref(database, 'users');
+
+    usersRef.orderByChild('userName').equalTo(userName).once('value')
+        .then(function (snapshot) {
+            if (snapshot.exists()) {
+                // Get the user data
+                const userData = snapshot.val();
+
+                // Assuming there's only one user with the specified userName
+                const userId = Object.keys(userData)[0];
+
+                // Call the callback function with the user data
+                callback(userId, userData[userId]);
+            } else {
+                // User with the specified userName does not exist
+                callback(null);
+            }
+        })
+        .catch(function (error) {
+            console.error("Error retrieving user by userName:", error);
+            callback(null); // Assume an error occurred
+        });
+}
+
+btnCalculateWaterTax.addEventListener('click', function (e) {
+    const connections = parseFloat(NumberOfConnections.value);
+    const volume = parseFloat(WaterVolume.value);
+    const rate = parseFloat(WaterRate.value);
+    const userAdhar = user_adhar.value;
+
+    // Check if the input values are valid numbers
+    if (isNaN(connections) || isNaN(volume) || isNaN(rate)) {
+        alert("Please enter valid numbers for all fields.");
+        return;
+    }
+
+    // Calculate the water tax
+    const waterTax = connections * volume * rate;
+
+    // Retrieve user data by userName
+    getUserByUserName(userAdhar, function (userId, userData) {
+        if (userId) {
+            // Use userId in the path instead of userName
+            saveTaxToFirebase(userId, waterTax);
+        } else {
+            console.log("User not found with userName:", userAdhar);
+        }
+    });
+});
+
+// Function to save tax information to Firebase
+function saveTaxToFirebase(userId, taxAmount) {
     const d = new Date();
     let currentMonthIndex = d.getMonth() + 1;
     let currentYear = d.getFullYear();
 
-    // Reference to the user's tax data
-    var userTaxRef = ref(database, `users/${userName}/waterTax/${currentMonthIndex}-${currentYear}`);
+    // Reference to the user's tax data using userId
+    var userTaxRef = ref(database, `users/${userId}/waterTax/${currentMonthIndex}-${currentYear}`);
 
     // Push the new tax information to the user's tax data
     set(userTaxRef, {
@@ -64,5 +135,5 @@ function saveTaxToFirebase(userName, taxAmount) {
     });
 
     // Display success message or perform any other actions
-    console.log("Tax information saved to Firebase for user with userName: " + userName);
+    console.log("Tax information saved to Firebase for user with userId: " + userId);
 }
--- VillagerHistoryPayment.js
+++ VillagerHistoryPayment.js
@@ -1,12 +1,53 @@
+// Firebase initialization
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js";
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDi3DAqMDFQiSak9MABrPz1W4r7_kpID2o",
+  authDomain: "capstone-project-fa003.firebaseapp.com",
+  databaseURL: "https://capstone-project-fa003-default-rtdb.firebaseio.com",
+  projectId: "capstone-project-fa003",
+  storageBucket: "capstone-project-fa003.appspot.com",
+  messagingSenderId: "257374439877",
+  appId: "1:257374439877:web:0909d4c9c34c7ffb48a3ac",
+  measurementId: "G-X1WGR3RS0G"
+};
 
-// JavaScript function to toggle visibility of a specific compartment
-function toggleCompartment(compartment) {
-    var compartmentElement = document.querySelector('.compartment.' + compartment);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app); // Initialize Firebase Auth
+const database = getDatabase();
 
-    if (compartmentElement.style.display === 'block') {
-        compartmentElement.style.display = 'none';
-    } else {
-        compartmentElement.style.display = 'block';
-    }
+// Function to display user data on the dashboard
+function displayUserData(user) {
+  // Get a reference to the database
+  const database = getDatabase();
+
+  // Assuming you have a 'users' collection in your database
+  const userRef = ref(database, 'users/' + user.uid);
+  console.log(user.uid);
+  // Listen for changes in user data
+  onValue(userRef, (snapshot) => {
+    const userData = snapshot.val();
+
+    console.log(userData);
+    // Display user data on the dashboard
+    // const welcomeMessageElement = document.getElementById('welcomeMessage');
+    // welcomeMessageElement.textContent = `Welcome, ${userData.userName}! Your email is ${userData.email}.`;
+  });
 }
+
+// Check if a user is signed in
+auth.onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in
+    displayUserData(user);
+  } else {
+    // No user is signed in
+    console.log('No user is signed in.');
+  }
+});
