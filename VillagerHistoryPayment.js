fe91355b3a9ee375c0ebf9a80d2c140309994bfb
keval <keval.waghate@gmail.com>
2023-11-28 14:09:55 +0530

    button convdesrion

--- VillagerHistoryPayment.js
+++ VillagerHistoryPayment.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-analytics.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
-import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js";
+import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -22,43 +22,73 @@ const analytics = getAnalytics(app);
 const auth = getAuth(app); // Initialize Firebase Auth
 const database = getDatabase();
 
-// Function to display tax and penalty calculations
+
+// Function to display tax and penalty calculations in a table
 // function displayCalculations(user, type_of_tax) {
+
 //   const userRef = ref(database, `users/${user.uid}/Transaction/${type_of_tax}`);
-//   const list = document.getElementById(type_of_tax);
+//   const table = document.getElementById(type_of_tax);
+
+
 
 //   get(userRef).then((snapshot) => {
 //     if (snapshot.exists()) {
 //       const calculation_data = snapshot.val();
 
 //       // Clear existing content
-//       list.innerHTML = '';
+//       table.innerHTML = '';
 
+//       // Create table headers
+//       const headerRow = document.createElement('tr');
+//       const dateHeader = document.createElement('th');
+//       dateHeader.textContent = 'Date';
+//       headerRow.appendChild(dateHeader);
+//       const amountHeader = document.createElement('th');
+//       amountHeader.textContent = 'Amount Payable';
+//       headerRow.appendChild(amountHeader);
+//       const actionHeader = document.createElement('th');
+//       actionHeader.textContent = 'Action';
+//       headerRow.appendChild(actionHeader);
+//       table.appendChild(headerRow);
+
+//       // Populate table rows
 //       for (const data in calculation_data) {
 //         const calculation = calculation_data[data];
-//         const listItem = document.createElement('li');
-//         listItem.textContent = `date : ${calculation.currentMonthIndex} paymable amount : ${calculation.taxAmount}`;
+//         const row = document.createElement('tr');
 
-//         // Create a button for each entry
-//         const printButton = document.createElement('button');
-//         printButton.textContent = 'pay';
-//         printButton.addEventListener('click', () => {
-//           // Print the amount to the console
+//         // Date column
+//         const dateCell = document.createElement('td');
+//         dateCell.textContent = calculation.currentMonthIndex;
+//         row.appendChild(dateCell);
+
+//         // Amount Payable column
+//         const amountCell = document.createElement('td');
+//         amountCell.textContent = calculation.taxAmount;
+//         row.appendChild(amountCell);
+
+//         // Action column (button)
+//         const actionCell = document.createElement('td');
+//         const payButton = document.createElement('button');
+//         payButton.textContent = 'Pay';
+//         payButton.addEventListener('click', () => {
+//           // Print the amount to the console or perform other actions
 //           console.log(`Amount for ${type_of_tax}: ${calculation.taxAmount}`);
+
+
+//           makeTransaction(type_of_tax, calculation.taxAmount, user,);
+
 //         });
+//         actionCell.appendChild(payButton);
+//         row.appendChild(actionCell);
 
-//         listItem.appendChild(printButton);
-
-//         // Append to the correct list
-//         list.appendChild(listItem);
+//         // Append the row to the table
+//         table.appendChild(row);
 //       }
 //     } else {
 //       console.error(`No ${type_of_tax} calculations found in the database`);
 //     }
 //   });
 // }
-
-// Function to display tax and penalty calculations in a table
 function displayCalculations(user, type_of_tax) {
   const userRef = ref(database, `users/${user.uid}/Transaction/${type_of_tax}`);
   const table = document.getElementById(type_of_tax);
@@ -98,19 +128,21 @@ function displayCalculations(user, type_of_tax) {
         amountCell.textContent = calculation.taxAmount;
         row.appendChild(amountCell);
 
-        // Action column (button)
+        // Action column (button or text)
         const actionCell = document.createElement('td');
-        const payButton = document.createElement('button');
-        payButton.textContent = 'Pay';
-        payButton.addEventListener('click', () => {
-          // Print the amount to the console or perform other actions
-          console.log(`Amount for ${type_of_tax}: ${calculation.taxAmount}`);
-
-
-          makeTransaction(type_of_tax ,calculation.taxAmount,);
-
-        });
-        actionCell.appendChild(payButton);
+        if (calculation.payment === 'paid') {
+          // If payment is already paid, display a message or different text
+          actionCell.textContent = 'Paid';
+        } else {
+          // If payment is not paid, display the "Pay" button
+          const payButton = document.createElement('button');
+          payButton.textContent = 'Pay';
+          payButton.addEventListener('click', () => {
+            console.log(`Amount for ${type_of_tax}: ${calculation.taxAmount}`);
+            makeTransaction(type_of_tax, calculation.taxAmount, user);
+          });
+          actionCell.appendChild(payButton);
+        }
         row.appendChild(actionCell);
 
         // Append the row to the table
@@ -121,6 +153,7 @@ function displayCalculations(user, type_of_tax) {
     }
   });
 }
+
 
 // Check if a user is signed in
 auth.onAuthStateChanged(function (user) {
@@ -136,21 +169,7 @@ auth.onAuthStateChanged(function (user) {
 });
 
 
-// Check if a user is signed in
-auth.onAuthStateChanged(function (user) {
-  if (user) {
-    // Display tax calculations
-    displayCalculations(user, 'waterTax');
-
-    // Display penalty calculations
-    displayCalculations(user, 'Penalty');
-  } else {
-    console.log('No user is signed in.');
-  }
-});
-
-
-function makeTransaction(disription, amount) {
+function makeTransaction(disription, amount, user) {
 
   var options = {
     "key": "rzp_test_UOCSpmDWDIAYLg", // Enter the Key ID generated from the Dashboard
@@ -169,25 +188,32 @@ function makeTransaction(disription, amount) {
     },// Replace this with the order_id created using Orders API (https://razorpay.com/docs/api/orders).
     "description": `${disription}`,
     "handler": function (response) {
-      savetoDB(response);
+      savetoDB(response, user, disription, amount);
       $('#myModal').modal();
     },
-    // "prefill": {
-    //     "name": `${user_actual_name}`,
-    //     "email": `${user_email}`,
-    //     "contact": `${user_phone_number}`
-    // },
+    "prefill": {
+      "name": `${user.userName}`,
+      "email": `${user.email}`,
+      "contact": `${user.mobileNum}`
+    },
   }
   var propay = new Razorpay(options);
   propay.open();
 };
 
 
-function savetoDB(response, type_of_tax) {
+function savetoDB(response, user, disription, taxAmount) {
   // console.log(response)
-  var payRef = ref(database, `users/uid/Transaction/${type_of_tax}/${date}`);
+  const d = new Date();
+  let currentMonthIndex = d.getMonth() + 1;
+  let currentYear = d.getFullYear();
+  let date = `${currentMonthIndex}-${currentYear}`
 
-  payRef.set({
+  var payRef = ref(database, `users/${user.uid}/Transaction/${disription}/${date}`);
+
+  set(payRef, {
+    currentMonthIndex: `${currentMonthIndex}.${currentYear}`,
+    taxAmount: taxAmount,
     payment: "paid",
     payment_id: response.razorpay_payment_id
   })
